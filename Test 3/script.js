e48349a7d3643da622926ab2ceeed750a76eef66
Nafis Handoko <naffisioner@gmail.com>
2023-01-14 11:24:46 +0700

    remove "you^1000" word in script.js

--- Test 3/script.js
+++ Test 3/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#role-text', {
-    strings: ['You^1000' ,'Application', 'Mobile', 'Website'],
+    strings: ['Application', 'Mobile', 'Website'],
     loop: true,
     typeSpeed: 100,
     backSpeed: 100
